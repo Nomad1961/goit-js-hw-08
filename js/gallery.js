61b318a6efcf6f972297c68f58a2adaf911aa684
Seanomad61 <161769498+Nomad1961@users.noreply.github.com>
2024-04-16 20:47:36 +0300

    Summary

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,4 @@
-const galleryContainer = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery');
 const images = [
   {
     preview:
@@ -14,39 +14,89 @@ const images = [
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
     description: 'Container Haulage Freight',
   },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
 ];
 
-// Create gallery markup
-const galleryMarkup = images.reduce(
-  (markup, { preview, original, description }) => {
-    return (
-      markup +
-      `
-    <li class="gallery-item">
-      <a class="gallery-link" href="${original}">
-        <img class="gallery-image" src="${preview}" data-source="${original}" alt="${description}" />
-      </a>
-    </li>
-  `
-    );
-  },
-  ''
-);
+const createGalleryItem = ({ preview, original, description }) => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery-item');
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+  const link = document.createElement('a');
+  link.classList.add('gallery-link');
+  link.href = original;
 
-// Event delegation for opening modal
-galleryContainer.addEventListener('click', e => {
-  e.preventDefault();
-  if (e.target.nodeName !== 'IMG') {
+  const image = document.createElement('img');
+  image.classList.add('gallery-image');
+  image.src = preview;
+  image.dataset.source = original;
+  image.alt = description;
+
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+
+  return galleryItem;
+};
+
+const onGalleryClick = event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
     return;
   }
-  const largeImageUrl = e.target.dataset.source;
-  console.log(largeImageUrl); // Output the large image URL to console
-});
 
-// Include basicLightbox library
-const script = document.createElement('script');
-script.src =
-  'https://cdn.jsdelivr.net/npm/basiclightbox/dist/basicLightbox.min.js';
-document.body.appendChild(script);
+  const largeImageURL = event.target.dataset.source;
+  const instance = basicLightbox.create(`<img src="${largeImageURL}">`);
+  instance.show();
+};
+
+gallery.addEventListener('click', onGalleryClick);
+
+const galleryItems = images.map(createGalleryItem);
+gallery.append(...galleryItems);
